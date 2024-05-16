153b777edd131958d30f4ec57f4b6e3e1000078b
tejada <camilotejada1@gmail.com>
2024-05-16 14:54:10 -0500

    Implementacion de swagger para comentar la api y sus controllers

--- Admin/src/main/java/com/riwi/admin_riwi/api/controllers/PsychologistController.java
+++ Admin/src/main/java/com/riwi/admin_riwi/api/controllers/PsychologistController.java
@@ -3,6 +3,7 @@ package com.riwi.admin_riwi.api.controllers;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +19,22 @@ import com.riwi.admin_riwi.api.dto.request.PsychologistReq;
 import com.riwi.admin_riwi.api.dto.response.PsychologistResponse;
 import com.riwi.admin_riwi.infrastructure.abstract_services.IPsychologistService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/psychologist")
 @AllArgsConstructor
 @CrossOrigin(origins = "*")
+@Tag(name = "psychologist")
 public class PsychologistController {
     private final IPsychologistService service;
 
+    @Operation(summary = "Lista todas las Psicologas dentro de la api ", description = "Se debe enviar la pagina y el tamaño de la pagina para recibir todas la vacantes correspondientes")
     @GetMapping
     public ResponseEntity<Page<PsychologistResponse>> findAll(@RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "5") int size) {
@@ -34,26 +42,36 @@ public class PsychologistController {
         return ResponseEntity.ok(service.getAll(page - 1, size));
     }
 
-    @GetMapping(path="/{id}")
-    public ResponseEntity<PsychologistResponse>getId(@PathVariable String id){
+    @ApiResponse(responseCode = "400", description = "Cuando el id no es válido", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+    })
+    @Operation(summary = "Lista una Psicologa por ID", description = "Se debe enviar el ID a buscar")
 
-            return ResponseEntity.ok(service.get(id));
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<PsychologistResponse> getId(@PathVariable String id) {
+
+        return ResponseEntity.ok(service.get(id));
     }
+    @Operation(summary = "Crea una Psicologa", description = "Crea un Psicologas se debe enviar la informacion en formato JSON")
     @PostMapping
-    public ResponseEntity<PsychologistResponse> create(@Validated @RequestBody PsychologistReq request){
+    public ResponseEntity<PsychologistResponse> create(@Validated @RequestBody PsychologistReq request) {
 
         return ResponseEntity.ok(service.create(request));
 
     }
+    @Operation(summary = "Elimina un Psicologa", description = "Se debe enviar el ID del Psicologa a eliminar por URL")
 
-    @DeleteMapping(path="/{id}")
-    public ResponseEntity<Void> delete(@PathVariable String id){
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
         this.service.delete(id);
         return ResponseEntity.noContent().build();
 
     }
-    @PutMapping(path="/{id}")
-    public ResponseEntity<PsychologistResponse> put(@PathVariable String id,@Validated @RequestBody PsychologistReq request){
+    @Operation(summary = "Edita la informacion de una Psicologa por ID", description = "El ID a editar de debe enviar por URL Y la informacion a enviar por body en formato Json")
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<PsychologistResponse> put(@PathVariable String id,
+            @Validated @RequestBody PsychologistReq request) {
 
         return ResponseEntity.ok(service.update(request, id));
     }
--- Admin/src/main/java/com/riwi/admin_riwi/api/controllers/RecordController.java
+++ Admin/src/main/java/com/riwi/admin_riwi/api/controllers/RecordController.java
@@ -3,6 +3,7 @@ package com.riwi.admin_riwi.api.controllers;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,48 +19,66 @@ import com.riwi.admin_riwi.api.dto.request.RecordRequest;
 import com.riwi.admin_riwi.api.dto.response.RecordResponse;
 import com.riwi.admin_riwi.infrastructure.abstract_services.IRecordService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/records")
 @AllArgsConstructor
 @CrossOrigin(origins = "*")
+@Tag(name = "Records")
 public class RecordController {
-    
+
     private final IRecordService recordService;
+
+    @Operation(summary = "Lista de todos los registros dentro de la api ", description = "Se debe enviar la pagina y el tamaño de la pagina para recibir todas la vacantes correspondientes")
 
     @GetMapping
     public ResponseEntity<Page<RecordResponse>> getAll(
-        @RequestParam(defaultValue = "1") int page,
-        @RequestParam(defaultValue = "10") int size){
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size) {
 
-            return ResponseEntity.ok(this.recordService.getAll(page - 1, size));
+        return ResponseEntity.ok(this.recordService.getAll(page - 1, size));
     }
+
+    @ApiResponse(responseCode = "400", description = "Cuando el id no es válido", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+    })
+    @Operation(summary = "Lista un Registro por ID", description = "Se debe enviar el ID a buscar")
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<RecordResponse> get(
-        @PathVariable Long id){
-            return ResponseEntity.ok(this.recordService.get(id));
+            @PathVariable Long id) {
+        return ResponseEntity.ok(this.recordService.get(id));
     }
+
+    @Operation(summary = "Crea un Registro", description = "Crea un Registro se debe enviar la informacion en formato JSON")
 
     @PostMapping
     public ResponseEntity<RecordResponse> insert(
-        @Validated @RequestBody RecordRequest request){
-            return ResponseEntity.ok(this.recordService.create(request));
+            @Validated @RequestBody RecordRequest request) {
+        return ResponseEntity.ok(this.recordService.create(request));
     }
+
+    @Operation(summary = "Edita la informacion de un registro por ID", description = "El ID a editar de debe enviar por URL Y la informacion a enviar por body en formato Json")
 
     @PutMapping(path = "/{id}")
     public ResponseEntity<RecordResponse> update(
-        @Validated @RequestBody RecordRequest request,
-        @PathVariable Long id){
-            return ResponseEntity.ok(this.recordService.update(request, id));
+            @Validated @RequestBody RecordRequest request,
+            @PathVariable Long id) {
+        return ResponseEntity.ok(this.recordService.update(request, id));
     }
 
+    @Operation(summary = "Elimina un Registro", description = "Se debe enviar el ID del Psicologa a eliminar por URL")
+
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         this.recordService.delete(id);
         return ResponseEntity.noContent().build();
     }
-
 
 }
