7ba827ec960b474b78981238b8b9e8daedec3651
tejada <camilotejada1@gmail.com>
2024-05-20 21:00:58 -0500

    se agregan nuevas propiedaes

--- Admin/src/main/java/com/riwi/admin_riwi/api/dto/request/RecordBasicResponse.java
+++ Admin/src/main/java/com/riwi/admin_riwi/api/dto/request/RecordBasicResponse.java
@@ -1,7 +1,5 @@
 package com.riwi.admin_riwi.api.dto.request;
 
-import java.time.LocalDate;
-
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -14,16 +12,10 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class RecordRequest {
-    
-    @FutureOrPresent(message = "La fecha y hora debe ser futura")
-    @NotNull(message = "La fecha y hora de la cita es requeridas")
-    private LocalDate dateRecord;
+public class RecordBasicResponse {
     @NotBlank(message = "El registro es requerido")
     private String registration;
     @NotBlank(message = "La observacion es requerida")
     private String observation;
-    @NotBlank(message = "El id del coder es requerido") 
-    private String coderId;
 
 }
